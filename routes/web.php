d1428c8ed431a7ad3cb850835e826065b0abe1ef
“MohamedFathi” <mohamedkdr66@yahoo.com>
2022-09-04 18:36:18 +0200

    Add localizations middlewares for routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +23,18 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group([
-    'as' => 'admin.',
-    'prefix' => 'admin',
-    'middleware' => 'role:admin',
-], function () {
-    require('web/admin.php');
-});
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+        Route::group([
+            'as' => 'admin.',
+            'prefix' => 'admin',
+            'middleware' => 'role:admin',
+        ], function () {
+            require('web/admin.php');
+        });
+    }
+);
