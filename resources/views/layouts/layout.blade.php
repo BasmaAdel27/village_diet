ebe46837ec5855ab166809d010222f6b38f386cd
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-09-05 22:59:36 +0200

    Change Login view and add redirection to dashboard if user is admin

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>@lang('site.admin-panel')</title>
+  <link rel="stylesheet" href="{{ asset('adminPanel/vendors/mdi/css/materialdesignicons.min.css') }}" />
+  <link rel="stylesheet" href="{{ asset('adminPanel/vendors/base/vendor.bundle.base.css') }}" />
+  <link rel="stylesheet" href="{{ asset('adminPanel/css/style.css') }}" />
+  <link rel="shortcut icon" href="{{ asset('adminPanel/images/favicon.png') }}" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        @yield('content')
+      </div>
+    </div>
+  </div>
+
+  <script src="{{ asset('adminPanel/vendors/base/vendor.bundle.base.js') }}"></script>
+  <script src="{{ asset('adminPanel/js/off-canvas.js') }}"></script>
+  <script src="{{ asset('adminPanel/js/hoverable-collapse.js') }}"></script>
+  <script src="{{ asset('adminPanel/js/template.js') }}"></script>
+</body>
+
+</html>
--- routes/web/admin.php
+++ routes/web/admin.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'admin.app');
-Route::get('/users/list',[UserController::class,'getUsers'])->name('users.list');
+Route::view('dashboard', 'admin.app')->name('dashboard');
+Route::get('/users/list', [UserController::class, 'getUsers'])->name('users.list');
 
 Route::resource('users', UserController::class);
