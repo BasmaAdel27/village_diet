54b29e782463fe0b4350c43e618e6a6814c7770d
“MohamedFathi” <mohamedkdr66@yahoo.com>
2022-09-09 18:07:19 +0200

    Add permissions to RatingController

--- app/Http/Controllers/Admin/RatingController.php
+++ app/Http/Controllers/Admin/RatingController.php
@@ -8,6 +8,11 @@ use App\Http\Controllers\Controller;
 
 class RatingController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('permission:admin.ratings.index');
+    }
+
     public function __invoke(RatingDatatable $ratingDatatable)
     {
         return $ratingDatatable->render('admin.ratings.index');
