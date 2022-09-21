49ad184f236cc3d927dbb4bffd941586aad29cc1
mohamed karem <m.karem456@gmail.com>
2022-09-21 13:21:03 +0000

    hide breadcrumb in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,11 +8,11 @@
       <div class="d-flex align-items-end flex-wrap">
         <div class="mr-md-3 mr-xl-5">
         </div>
-        <div class="d-flex">
-          <i class="mdi mdi-home text-muted hover-cursor"></i>
-          <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
-          <p class="text-primary mb-0 hover-cursor">Analytics</p>
-        </div>
+        // <div class="d-flex">
+        //   <i class="mdi mdi-home text-muted hover-cursor"></i>
+        //   <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
+        //   <p class="text-primary mb-0 hover-cursor">Analytics</p>
+        // </div>
       </div>
     </div>
   </div>
