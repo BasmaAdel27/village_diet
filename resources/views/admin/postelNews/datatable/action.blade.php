f23874919c2ad588008e122f0f0a8c33e52aecf7
“MohamedFathi” <mohamedkdr66@yahoo.com>
2022-09-23 10:21:13 +0200

    Delete unused postel_news folder

--- resources/views/admin/postelNews/datatable/action.blade.php
+++
@@ -1,6 +0,0 @@
-<a href="{{ route('admin.PostelNews.edit',$query->id) }}" class="btn btn-outline-success mr-2 p-2">@lang('edit')</a>
-<button type="submit" class="btn btn-outline-danger mr-2 p-2 " form="DeleteForm"
-        onclick="DeleteElement(this)">@lang('delete')</button>
-<form action="{{ route('admin.PostelNews.destroy',$query->id) }}" id="DeleteForm" method="POST">@method('delete')
-  @csrf
-</form>
