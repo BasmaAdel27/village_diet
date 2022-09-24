f2f82cb01325dbabe19083fb41a03ba6c7888211
basma adel <basmaadelmohamed@outlook.com>
2022-09-24 18:34:07 +0200

    contact us api

--- app/Http/Controllers/Api/ContactUsController.php
+++ app/Http/Controllers/Api/ContactUsController.php
@@ -18,6 +18,7 @@ class ContactUsController extends Controller
                 'user_type'=>'user'
           ]);
 
+
           return successResponse(ContactUsResource::make($contactUs));
     }
 }
