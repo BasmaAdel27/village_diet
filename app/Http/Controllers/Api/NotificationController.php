8525cc93aa3df4d16f5ce0fa3c1b91cf26cae405
“MohamedFathi” <mohamedkdr66@yahoo.com>
2022-10-13 21:56:34 +0200

    Fix issues

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -12,7 +12,7 @@ class NotificationController extends Controller
     public function index()
     {
         $notifications = auth()->user()->notifications()->paginate();
-        $notifications->map->markAsRead();
+        auth()->user()->notifications()->markAsRead();
 
         return successResponse(NotificationCollection::make($notifications));
     }
