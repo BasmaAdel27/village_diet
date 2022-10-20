d061effb82b041733181e00e5851612dcf056d4e
Mohamed <mohamedkdr66@yahoo.com>
2022-10-20 21:33:25 +0200

    add user firebase_id

---
+++ app/Http/Requests/Api/AddFirebaseRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddFirebaseRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'firebase_if' => 'required|string'
+        ];
+    }
+}
