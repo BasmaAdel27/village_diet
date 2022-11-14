8e283f489e7e1f6a21e7355aa6ff84dcd5744f7a
Mohamed <mohamedkdr66@yahoo.com>
2022-11-14 21:08:07 +0200

    Fix issue in loading server

--- resources/views/admin/layout/scripts.blade.php
+++ resources/views/admin/layout/scripts.blade.php
@@ -19,7 +19,7 @@
 @endif
 <script src="{{ asset('js/custom.js') }}"></script>
 <script src="{{ asset('js/printThis.js') }}"></script>
-<script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script>
+{{-- <script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script> --}}
 <script src="{{ asset('adminPanel/js/chat.js') }}"></script>
 <script>
   var element = document.getElementById("chat");
