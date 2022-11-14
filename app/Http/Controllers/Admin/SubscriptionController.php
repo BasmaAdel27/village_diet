48c8416efbcf801152beb986a758fb64f7812ac8
Mohamed <mohamedkdr66@yahoo.com>
2022-11-14 20:39:34 +0200

    Fix subscription

--- app/Http/Controllers/Admin/SubscriptionController.php
+++ app/Http/Controllers/Admin/SubscriptionController.php
@@ -5,38 +5,44 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\Admin\SubscriptionDatatable;
 use App\Http\Controllers\Controller;
 use App\Models\Subscription;
-use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
-    public  function index(SubscriptionDatatable $subscriptionDatatable){
+    public  function index(SubscriptionDatatable $subscriptionDatatable)
+    {
         return $subscriptionDatatable->render('admin.subscriptions.index');
     }
 
-    public function active($id){
-        $subscription=Subscription::find($id);
+    public function active($id)
+    {
+        $subscription = Subscription::find($id);
         $subscription->update([
-              'status' => Subscription::ACTIVE,
-              'status_ar'=>Subscription::ACTIVE_AR
+            'status' => Subscription::ACTIVE,
+            'status_ar' => Subscription::ACTIVE_AR
         ]);
-        return redirect()->back()->with('success',trans('updated_successfully'));
+
+        return redirect()->back()->with('success', trans('updated_successfully'));
     }
 
-    public function cancel($id){
-        $subscription=Subscription::find($id);
+    public function cancel($id)
+    {
+        $subscription = Subscription::find($id);
         $subscription->update([
-              'status' => Subscription::REQUEST_CANCEL,
-              'status_ar'=>Subscription::REQUEST_CANCEL_AR
+            'status' => Subscription::REQUEST_CANCEL,
+            'status_ar' => Subscription::REQUEST_CANCEL_AR
         ]);
-        return redirect()->back()->with('success',trans('updated_successfully'));
+
+        return redirect()->back()->with('success', trans('updated_successfully'));
     }
 
-    public function inactive($id){
-        $subscription=Subscription::find($id);
+    public function inactive($id)
+    {
+        $subscription = Subscription::find($id);
         $subscription->update([
-              'status' => Subscription::IN_ACTIVE,
-              'status_ar'=>Subscription::INACTIVE_AR
+            'status' => Subscription::IN_ACTIVE,
+            'status_ar' => Subscription::INACTIVE_AR
         ]);
-        return redirect()->back()->with('success',trans('updated_successfully'));
+
+        return redirect()->back()->with('success', trans('updated_successfully'));
     }
 }
