47c2192640b03616a575e5911940b383f4ca068b
mohamed karem <m.karem456@gmail.com>
2022-11-29 07:04:14 +0000

    Update app/Support/Helpers.php

--- app/Support/Helpers.php
+++ app/Support/Helpers.php
@@ -75,36 +75,100 @@ if (!function_exists('send_notification')) {
     function send_notification($token, $content, $title, $message)
     {
 
-        $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
-        $notification = [
-              'notification' => $message,
-              'sound' => true,
-              'title' => $title,
-              'body' => $content,
-              'priority' => 'high',
+
+    $firebaseToken = $token;
+
+
+
+       $server_key = config('auth.firebase_id');
+
+
+
+        $data = [
+
+            "registration_ids" => $firebaseToken,
+
+            "notification" => [
+
+                "title" => $title,
+
+                "body" => $content,
+
+                'notification' => $message,
+
+            ]
+
         ];
 
-        $extraNotificationData = ["data" => $notification];
+        $dataString = json_encode($data);
 
-        $fcmNotification = [
-              'to' => $token, //single token
-              'notification' => $notification,
-              'data' => $extraNotificationData
+
+
+        $headers = [
+
+            'Authorization: key=' . $SERVER_API_KEY,
+
+            'Content-Type: application/json',
+
         ];
-        $server_key = config('auth.firebase_id');
-        $headers = array(
-              'Content-Type:application/json',
-              'Authorization:key=' . $server_key
-        );
+
+
+
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $fcmUrl);
+
+
+
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+
         curl_setopt($ch, CURLOPT_POST, true);
+
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return true;
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+
+
+
+        $response = curl_exec($ch);
+
+
+
+        dd($response);
+
+
+        // $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+        // $notification = [
+        //       'notification' => $message,
+        //       'sound' => true,
+        //       'title' => $title,
+        //       'body' => $content,
+        //       'priority' => 'high',
+        // ];
+
+        // $extraNotificationData = ["data" => $notification];
+
+        // $fcmNotification = [
+        //       'to' => $token, //single token
+        //       'notification' => $notification,
+        //       'data' => $extraNotificationData
+        // ];
+        // $server_key = config('auth.firebase_id');
+        // $headers = array(
+        //       'Content-Type:application/json',
+        //       'Authorization:key=' . $server_key
+        // );
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $fcmUrl);
+        // curl_setopt($ch, CURLOPT_POST, true);
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
+        // $result = curl_exec($ch);
+        // curl_close($ch);
+        // return true;
     }
 }
