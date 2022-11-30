c0bae117864e0de3c20e3c6a2e91744cf2520426
mohamed <m.karem456@gmail.com>
2022-11-30 22:27:15 +0200

    fix notifications

--- app/Http/Resources/Api/Notification/NotificationResource.php
+++ app/Http/Resources/Api/Notification/NotificationResource.php
@@ -9,13 +9,15 @@ class NotificationResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'title' => @$this->data['title'],
-            'body' => @$this->data['body'],
-            'notify_type' => $this->type,
-            'created_at' => $this->created_at?->diffForHumans(),
-            'read_at' => optional($this->read_at)->format("Y-m-d H:i"),
-            'user_image' => auth()->user()->image
+              'id' => $this->id,
+              'type' => @$this->data['type'],
+              'title' => @$this->data['title'],
+              'body' => @$this->data['message'],
+              'society_id' => @$this->when(@$this->data['society_id'], @$this->data['society_id']),
+              'notify_type' => $this->type,
+              'created_at' => $this->created_at?->diffForHumans(),
+              'read_at' => optional($this->read_at)->format("Y-m-d H:i"),
+              'user_image' => auth()->user()->image
         ];
     }
 }
