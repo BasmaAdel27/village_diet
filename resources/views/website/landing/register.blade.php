8742f4a4feef62019da716a6a1a0312a7e96f92e
Mohamed Fathi <mohamedkdr66@gmail.com>
2022-12-03 00:42:24 +0200

    add trainer register in landing

--- resources/views/website/landing/register.blade.php
+++ resources/views/website/landing/register.blade.php
@@ -16,7 +16,7 @@
 
         <div class="col-lg-4 col-12">
           <div class="button-contain">
-            <a href="#" class="custom-btn gray-color">
+            <a href="{{ route('website.register_trainer.create') }}" class="custom-btn gray-color">
               <span>@lang('register_now')</span>
             </a>
           </div>
