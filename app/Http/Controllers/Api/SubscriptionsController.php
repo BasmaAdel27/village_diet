fac6eb08289d5cd3ddaf9f9444440abcbcb0639b
Mohamed Karem <m.karem456@gmail.com>
2023-01-15 23:39:13 +0200

    edits

--- app/Http/Controllers/Api/SubscriptionsController.php
+++ app/Http/Controllers/Api/SubscriptionsController.php
@@ -18,7 +18,7 @@ class SubscriptionsController extends Controller
         $data = new stdClass();
         $user = auth()->user();
 
-        $data->logs = $user->subscriptions()->where('id', '<>', $user->currentSubscription->id)->get();
+        $data->logs = $user->subscriptions()->where('id', '<>', $user->currentSubscription?->id)->get();
         $data->active_subscription = $user->currentSubscription;
         if ($user->currentSubscription->status == 'request_cancel' || $user->currentSubscription->status == 'in_active' && $user->currentSubscription->end_date <= now()) {
             $user->currentSubscription->update(['status' => Subscription::FINISHED]);
