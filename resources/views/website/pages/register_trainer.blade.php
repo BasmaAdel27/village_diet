0a45b82743a0ba8bc2d30fdd70f33dd54ca36140
Mohamed <mohamedkdr66@yahoo.com>
2023-01-15 22:41:18 +0200

    format register for trainer

--- resources/views/website/pages/register_trainer.blade.php
+++ resources/views/website/pages/register_trainer.blade.php
@@ -6,7 +6,7 @@
   <div class="container general-pattern right-left-pattern">
     <div class="heading">
       <h1>
-       @lang('register_as_trainer')
+        @lang('register_as_trainer')
       </h1>
 
     </div>
@@ -22,76 +22,65 @@
             <h1>
               @lang('register_as_trainer')
             </h1>
-
             <p>
               @lang('trainer_desc')
             </p>
           </div>
-
-          <form  method="post" enctype="multipart/form-data" class="form-contain" id="form"  action="register_trainer/store" >
+          <form method="post" enctype="multipart/form-data" class="form-contain" id="form"
+            action="register_trainer/store">
             @csrf
             <h2 class="sub-title">
               @lang('basic_informations')
             </h2>
-
             <div class="row">
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-group">
                   <img src="{{ asset('website/assets/images/form/user.svg') }}" class="icon" loading="lazy" alt="" />
 
-                  <input type="text" class="form-control" placeholder="@lang('first_name')" value="{{old('first_name')}}" name="first_name"/>
+                  <input type="text" class="form-control" placeholder="@lang('first_name')"
+                    value="{{old('first_name')}}" name="first_name" />
                 </div>
               </div>
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-group">
                   <img src="{{ asset('website/assets/images/form/user.svg') }}" class="icon" loading="lazy" alt="" />
-
-                  <input type="text" class="form-control" placeholder="@lang('last_name')"  name="last_name" value="{{old('last_name')}}"/>
+                  <input type="text" class="form-control" placeholder="@lang('last_name')" name="last_name"
+                    value="{{old('last_name')}}" />
                 </div>
               </div>
-
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-group">
                   <img src="{{ asset('website/assets/images/form/sms.svg') }}" class="icon" loading="lazy" alt="" />
-
-                  <input type="text" class="form-control" placeholder="@lang('email')" name="email"  value="{{old('email')}}"/>
+                  <input type="text" class="form-control" placeholder="@lang('email')" name="email"
+                    value="{{old('email')}}" />
                 </div>
               </div>
-
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-group">
-                  <img src="{{ asset('website/assets/images/form/phone.svg') }}" class="icon" loading="lazy"
-                       alt=""/>
-
-                  <input type="tel" value="{{ old('phone') ?? '' }}" name="phone"
-                         class="form-control" placeholder="@lang('phone')"/>
+                  <img src="{{ asset('website/assets/images/form/phone.svg') }}" class="icon" loading="lazy" alt="" />
+                  <input type="tel" value="{{ old('phone') ?? '' }}" name="phone" class="form-control"
+                    placeholder="@lang('phone')" />
                 </div>
               </div>
-
               <div class="col-lg-12 col-12 px-2">
                 <div class="form-group">
                   <img src="{{ asset('website/assets/images/form/idnoac.svg') }}" class="icon" loading="lazy" alt="" />
-
-                  <input type="text" class="form-control" placeholder="@lang('current_job')" name="current_job" value="{{old('current_job')}}" />
+                  <input type="text" class="form-control" placeholder="@lang('current_job')" name="current_job"
+                    value="{{old('current_job')}}" />
                 </div>
               </div>
-
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-data">
                   <label for="">
                     @lang('image')
                   </label>
-
                   <div class="file-upload">
-                    <input type="file" name="image" id="uploadFile" value="{{old('image')}}"/>
-
+                    <input type="file" name="image" id="uploadFile" value="{{old('image')}}" />
                     <label for="uploadFile" class="file-upload-section">
                       <img src="{{ asset('website/assets/images/form/document_upload.svg') }}" alt="">
-
                       <h2>
                         @lang('attach_image')
                       </h2>
-
                       <p>
                         @lang('max_size') : 2MB
                       </p>
@@ -99,23 +88,18 @@
                   </div>
                 </div>
               </div>
-
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-data">
                   <label for="">
                     @lang('cv')
                   </label>
-
                   <div class="file-upload">
                     <input type="file" name="cv" id="uploadFile2" value="{{old('cv')}}" />
-
                     <label for="uploadFile2" class="file-upload-section">
                       <img src="{{ asset('website/assets/images/form/document_upload.svg') }}" alt="">
-
                       <h2>
                         @lang('attach_cv')
                       </h2>
-
                       <p>
                         @lang('pdf_or_word')
                       </p>
@@ -123,7 +107,6 @@
                   </div>
                 </div>
               </div>
-
               <div class="col-12">
                 <div class="form-group text-area">
                   <textarea name="bio" class="form-control" placeholder=" @lang('write_about') ... " id="" cols="30"
@@ -131,175 +114,153 @@
                 </div>
               </div>
             </div>
-
             <h2 class="sub-title section-contain">
               @lang('location_information')
             </h2>
-
             <div class="row">
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-group select">
                   <img src="{{ asset('website/assets/images/form/flag.svg') }}" class="icon" loading="lazy" alt="" />
-
                   <select class="form-control" name="country_id" id="country">
                     <option value="" hidden>
                       @lang('country')
                     </option>
                     @foreach ($countries as $id => $name)
-                      <option value="{{$id}}" {{$id==old('country_id') ? 'selected' :''}}>{{trans($name)}}</option>
+                    <option value="{{$id}}" {{$id==old('country_id') ? 'selected' :''}}>{{trans($name)}}</option>
                     @endforeach
-
                   </select>
                 </div>
               </div>
-
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-group  state">
                   <img src="{{ asset('website/assets/images/form/location.svg') }}" class="icon" loading="lazy"
                     alt="" />
-
-                  <input type="text" name="city" class="form-control" value="" placeholder="@lang('city')" value="{{old('city')}}">
-
-
+                  <input type="text" name="city" class="form-control" value="" placeholder="@lang('city')"
+                    value="{{old('city')}}">
                   </select>
                 </div>
               </div>
-
               <div class="col-12">
                 <div class="form-group text-area no-margin">
-                  <textarea name="address" class="form-control" placeholder=" ... @lang('detailed_address')" id="" cols="30"
-                    rows="10">{{old('address')}}</textarea>
+                  <textarea name="address" class="form-control" placeholder=" ... @lang('detailed_address')" id=""
+                    cols="30" rows="10">{{old('address')}}</textarea>
                 </div>
               </div>
             </div>
-
             <h2 class="sub-title section-contain">
               @lang('social_media_accounts')
             </h2>
-
             <div class="row">
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-group">
                   <img src="{{ asset('website/assets/images/form/instagram_twotone.svg') }}" loading="lazy" alt=""
                     class="icon" />
-
-                  <input type="text" class="form-control" name="instagram" placeholder="@lang('instagram')" value="{{old('instagram')}}"/>
+                  <input type="text" class="form-control" name="instagram" placeholder="@lang('instagram')"
+                    value="{{old('instagram')}}" />
                 </div>
               </div>
-
               <div class="col-lg-6 col-12 px-2">
                 <div class="form-group">
                   <img src="{{ asset('website/assets/images/form/twitter.svg') }}" loading="lazy" alt="" class="icon" />
 
-                  <input type="text" class="form-control" name="twitter" placeholder="@lang('twitter')" value="{{old('twitter')}}" />
+                  <input type="text" class="form-control" name="twitter" placeholder="@lang('twitter')"
+                    value="{{old('twitter')}}" />
                 </div>
               </div>
             </div>
-
             <h2 class="sub-title section-contain">
               @lang('body shape')
             </h2>
-
             <div class="wrapper mb-1">
-              <input type="radio" class="radio-check" name="body_shape" value="slim" {{old('body_shape')=='slim' ? 'checked':''}}/>
-
+              <input type="radio" class="radio-check" name="body_shape" value="slim" {{old('body_shape')=='slim'
+                ? 'checked' :''}} />
               <label class="radio-title">
                 @lang('slim')
               </label>
             </div>
-
             <div class="wrapper mb-1">
-              <input type="radio" class="radio-check" name="body_shape" value="sportsman" {{old('body_shape')=='sportsman' ? 'checked':''}}/>
-
+              <input type="radio" class="radio-check" name="body_shape" value="sportsman"
+                {{old('body_shape')=='sportsman' ? 'checked' :''}} />
               <label class="radio-title">
                 @lang('sportsman')
               </label>
             </div>
-
             <div class="wrapper mb-1">
-              <input type="radio" class="radio-check" name="body_shape" value="Stretchy muscles" {{old('body_shape')=='Stretchy muscles' ? 'checked':''}}/>
+              <input type="radio" class="radio-check" name="body_shape" value="Stretchy muscles"
+                {{old('body_shape')=='Stretchy muscles' ? 'checked' :''}} />
 
               <label class="radio-title">
                 @lang('Stretchy_muscles')
               </label>
             </div>
-
             <div class="wrapper mb-1">
-              <input type="radio" class="radio-check" name="body_shape"  value="Medium build" {{old('body_shape')=='Medium build' ? 'checked':''}}/>
+              <input type="radio" class="radio-check" name="body_shape" value="Medium build"
+                {{old('body_shape')=='Medium build' ? 'checked' :''}} />
 
               <label class="radio-title">
                 @lang('Medium_build')
               </label>
             </div>
-
             <div class="wrapper mb-1">
-              <input type="radio" class="radio-check" name="body_shape" vlaue="Slightly overweight" {{old('body_shape')=='Slightly overweight' ? 'checked':''}}/>
+              <input type="radio" class="radio-check" name="body_shape" vlaue="Slightly overweight"
+                {{old('body_shape')=='Slightly overweight' ? 'checked' :''}} />
 
               <label class="radio-title">
                 @lang('Slightly_overweight')
               </label>
             </div>
             <div class="wrapper mb-1">
-              <input type="radio" class="radio-check"  name="body_shape" value="overweight" {{old('body_shape')=='overweight' ? 'checked':''}}/>
-
+              <input type="radio" class="radio-check" name="body_shape" value="overweight"
+                {{old('body_shape')=='overweight' ? 'checked' :''}} />
               <label class="radio-title">
                 @lang('overweight')
               </label>
             </div>
-
             <h3 class="sub-head">
               @lang('is licensed?')
             </h3>
-
             <div class="flex-data">
               <div class="wrapper mb-1">
-                <input type="radio" class="radio-check" name="is_certified" value="1" id="license" {{old('is_certified')==1 ? 'checked':''}}/>
+                <input type="radio" class="radio-check" name="is_certified" value="1" id="license"
+                  {{old('is_certified')==1 ? 'checked' :''}} />
 
                 <label class="radio-title">
                   @lang('yes')
                 </label>
               </div>
-
               <div class="wrapper mb-1">
-                <input type="radio" class="radio-check" name="is_certified" value="0" id="license1" {{old('is_certified')==0 ? 'checked':''}}/>
-
+                <input type="radio" class="radio-check" name="is_certified" value="0" id="license1"
+                  {{old('is_certified')==0 ? 'checked' :''}} />
                 <label class="radio-title">
                   @lang('no')
                 </label>
               </div>
             </div>
-
             <div class="form-data" id="license_img">
               <label for="">
                 @lang('licensed_image')
               </label>
-
               <div class="file-upload">
-                <input type="file" name="confidental_image" id="uploadFile3"  value="{{old('confidental_image')}}"/>
-
+                <input type="file" name="confidental_image" id="uploadFile3" value="{{old('confidental_image')}}" />
                 <label for="uploadFile3" class="file-upload-section">
                   <img src="{{ asset('website/assets/images/form/document_upload.svg') }}" alt="">
-
                   <h2>
                     @lang('attach_confidental_image')
                   </h2>
-
                   <p>
                     @lang('max_size') : 2MB
                   </p>
                 </label>
               </div>
             </div>
-
             <div class="form-group text-area">
-              <textarea name="join_request_reason" class="form-control" placeholder=" @lang("reason to join us").... " id="" cols="30"
-                rows="10">{{old('join_request_reason')}}</textarea>
+              <textarea name="join_request_reason" class="form-control" placeholder=" @lang(" reason to join
+                us").... " id="" cols=" 30" rows="10">{{old('join_request_reason')}}</textarea>
             </div>
 
             <div class="button-contain">
               <button type="submit" class="custom-btn" data-toggle="modal" id="btn-job-submit">
                 <img src="{{ asset('website/assets/images/icons/arrow.svg') }}" loading="lazy" alt="" />
-
                 <span>
                   @lang('register')
                 </span>
@@ -314,8 +275,8 @@
 
 @endsection
 @section('scripts')
-  <script>
-    $(document).ready(function () {
+<script>
+  $(document).ready(function () {
       $("#license_img").hide();
       $("input[name$='is_certified']").click(function() {
         var test = $(this).val();
@@ -373,7 +334,5 @@
     });
 
 
-  </script>
+</script>
 @endsection
-
-
