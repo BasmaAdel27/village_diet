0bd4632c321db35f23ac6d529a65f5040275fd3e
Mohamed Karem <m.karem456@gmail.com>
2023-01-24 11:16:17 +0200

    edits

--- app/Http/Requests/Admin/UserRequest.php
+++ app/Http/Requests/Admin/UserRequest.php
@@ -14,15 +14,15 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'first_name' => 'required',
-            'last_name' => 'required',
-            'email' => 'required|unique:users,email,' . $this->user?->id,
-            'phone' => 'required',
-            'date_of_birth' => 'required',
-            'country_id' => 'required',
-            'city' => 'nullable',
-            'address' => 'required',
-            'insta_link' => 'required',
+              'first_name' => 'required',
+              'last_name' => 'required',
+              'email' => 'required|unique:users,email,' . $this->user?->id,
+              'phone' => 'required',
+              'date_of_birth' => 'required',
+              'country_id' => 'required',
+              'city' => 'nullable',
+              'address' => 'nullable|string|max:255',
+              'insta_link' => 'required',
         ];
     }
 }
