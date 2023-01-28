9c029d07c36a0b022ebb041068d25665700ef201
Mohamed Karem <m.karem456@gmail.com>
2023-01-28 19:39:48 +0200

    edits

--- app/Http/Requests/Website/RegisterRequest.php
+++ app/Http/Requests/Website/RegisterRequest.php
@@ -12,12 +12,12 @@ class RegisterRequest extends FormRequest
               'first_name' => 'required|max:255|min:2|string',
               'last_name' => 'required|max:255|min:3|string',
               'email' => 'required|email|confirmed|unique:users,email,' . $this->id . ',id,step,3',
-              'phone' => 'required|numeric',
+              'phone' => 'required|numeric|max:11|starts_with:05',
               'date_of_birth' => 'required|date|date_format:Y-m-d|before:today',
               'country_id' => 'required|exists:countries,id',
               'city' => 'required|string',
               'address' => 'nullable|string|max:255',
-              'insta_link' => 'required|string',
+              'insta_link' => 'nullable|string|max:20',
               'is_postal' => 'nullable|in:1,0',
         ];
     }
